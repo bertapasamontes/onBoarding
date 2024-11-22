29e792ad0eab3c5f20b2f58852ed75f00ad487c6
bertapasamontes <bertapasamontes@gmail.com>
2024-11-22 16:03:33 +0100

    ex.3

--- src/app/services/steps.service.ts
+++ src/app/services/steps.service.ts
@@ -10,14 +10,20 @@ export class StepsService {
     {
       title:"Dedica moltes hores",
       description: "Un mínim de 30 hores a la setmana. Si no en tens prou, hauràs de dedicar-li més hores. Al principi sembla impossible, però notaràs una millora ràpidament.",
+      img:"https://raw.githubusercontent.com/IT-Academy-BCN/sprints-frontend-assets/ae4c30ae2dbeba5e2e884d99f426842b5573b585/sprint-5/time_managment.svg",
+      bgcolor:"#FABC3C",
     },
     {
       title: "Programa projectes propis:",
       description:"Més val 10 hores treballant en projectes propis, que 10 hores mirant tutorials. La motivació i la implicació en el projecte ajudarà a accelerar el teu aprenentatge.",
+      img:"https://raw.githubusercontent.com/IT-Academy-BCN/sprints-frontend-assets/ae4c30ae2dbeba5e2e884d99f426842b5573b585/sprint-5/programming.svg",
+      bgcolor:"#A1B5D8",
     },
     {
       title: "Procura descansar:",
       description:"Descansar bé i desconnectar són vitals. D'aquesta manera reduiràs l'estrès i l'ansietat. Milloraràs la teva concentració i consolidaràs el teu aprenentatge.",
+      img: "https://raw.githubusercontent.com/IT-Academy-BCN/sprints-frontend-assets/ae4c30ae2dbeba5e2e884d99f426842b5573b585/sprint-5/meditation.svg",
+      bgcolor:"#E4FDE1",
     }
   ];
   // constructor() {}
